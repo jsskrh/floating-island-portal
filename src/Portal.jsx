9132e19a258cb33fe3ad8259a2b4da85b8ccbe55
Jesse Akorah <jesseakorah@gmail.com>
2022-11-24 00:30:38 +0100

    Add galaxy mask to portal

--- src/Portal.jsx
+++ src/Portal.jsx
@@ -1,11 +1,42 @@
-import { useLoader } from "@react-three/fiber";
+import { useFrame, useLoader } from "@react-three/fiber";
 import React, { useEffect } from "react";
-import { DoubleSide } from "three";
+import {
+  AlwaysStencilFunc,
+  DoubleSide,
+  EquirectangularReflectionMapping,
+  LinearEncoding,
+  ReplaceStencilOp,
+  Scene,
+  TextureLoader,
+  WebGLRenderTarget,
+} from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import FillQuad from "./FillQuad";
+
+const scene = new Scene();
+scene.background = new TextureLoader().load(
+  "/textures/galaxy.jpg",
+  (texture) => {
+    texture.encoding = LinearEncoding;
+    texture.mapping = EquirectangularReflectionMapping;
+  }
+);
+
+const target = new WebGLRenderTarget(window.innerWidth, window.innerHeight);
+
+window.addEventListener("resize", () => {
+  target.setSize(window.innerWidth, window.innerHeight);
+});
 
 const Portal = () => {
   const model = useLoader(GLTFLoader, "/models/portal.glb");
   const mask = useLoader(GLTFLoader, "/models/portal_mask.glb");
+
+  useFrame((state) => {
+    state.gl.setRenderTarget(target);
+    state.gl.render(scene, state.camera);
+    state.gl.setRenderTarget(null);
+  });
 
   useEffect(() => {
     if (!model) return;
@@ -15,12 +46,18 @@ const Portal = () => {
 
     let maskMesh = mask.scene.children[0];
     maskMesh.material.side = DoubleSide;
+    maskMesh.material.transparent = false;
+    maskMesh.material.stencilWrite = true;
+    maskMesh.material.stencilRef = 1;
+    maskMesh.material.stencilFunc = AlwaysStencilFunc;
+    maskMesh.material.stencilZPass = ReplaceStencilOp;
   }, [model, mask]);
 
   return (
     <>
       <primitive object={model.scene} />
       <primitive object={mask.scene} />
+      <FillQuad map={target.texture} maskId={1} />
     </>
   );
 };
